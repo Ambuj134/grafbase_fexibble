5d87d74e5d860b2ec9f095e05327c344c06396f4
Ambuj134 <ambuj01tiwari@gmail.com>
2023-07-03 12:32:04 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover remarkable developer projects',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
